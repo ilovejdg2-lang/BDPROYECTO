cc5155c210e60e8d7945111e704317e2cbaca921
Brayan Pérez <128260780+BrayanperezBalladares@users.noreply.github.com>
2026-06-08 00:48:07 -0600

    Update database creation script with new parameters

--- Sprints/1_CrearBDyFG.sql
+++ Sprints/1_CrearBDyFG.sql
@@ -5,16 +5,23 @@
 
 --  crear la base (primary + log) 
 USE master;
+/* ============================================================================
+   PROYECTO: INSTITUTO TECNICO "TECNIC"   -  ARCHIVO 1/7
+   CREACION DE BASE, FILEGROUPS
+   ============================================================================ */
+
+--  crear la base (primary + log) 
+USE master;
 GO
 CREATE DATABASE InstitutoTECNIC
 ON PRIMARY
 ( NAME = 'TECNIC_Data',
-  FILENAME = 'C:\SQLData\TECNIC_Data.mdf',
-  SIZE = 25MB, MAXSIZE = 250MB, FILEGROWTH = 5MB )
+  FILENAME = 'C:\SQL Data\TECNIC_Data.mdf',
+  SIZE = 128MB, MAXSIZE = 1024MB, FILEGROWTH = 64MB )
 LOG ON
 ( NAME = 'TECNIC_Log',
-  FILENAME = 'C:\SQLData\TECNIC_Log.ldf',
-  SIZE = 10MB, MAXSIZE = 45MB, FILEGROWTH = 4MB );
+  FILENAME = 'C:\SQL LOG\TECNIC_Log.ldf',
+  SIZE = 64MB, MAXSIZE = 1024MB, FILEGROWTH = 64MB );
 GO
 
 --  filegroups y sus archivos 
@@ -25,9 +32,11 @@ GO
 USE master;
 GO
 ALTER DATABASE InstitutoTECNIC ADD FILE
-( NAME = 'TECNIC_Academico1', FILENAME = 'C:\SQLData\TECNIC_Academico1.ndf',
-  SIZE = 15MB, MAXSIZE = 150MB, FILEGROWTH = 5MB ) TO FILEGROUP FG_Academico;
+( NAME = 'TECNIC_Academico1', FILENAME = 'C:\SQL Data\TECNIC_Academico1.ndf',
+  SIZE = 256MB, MAXSIZE = 2048MB, FILEGROWTH = 128MB ) TO FILEGROUP FG_Academico;
 GO
+
+
 USE master;
 GO
 ALTER DATABASE InstitutoTECNIC ADD FILEGROUP FG_Operaciones;
@@ -35,9 +44,11 @@ GO
 USE master;
 GO
 ALTER DATABASE InstitutoTECNIC ADD FILE
-( NAME = 'TECNIC_Operaciones1', FILENAME = 'C:\SQLData\TECNIC_Operaciones1.ndf',
-  SIZE = 15MB, MAXSIZE = 150MB, FILEGROWTH = 5MB ) TO FILEGROUP FG_Operaciones;
+( NAME = 'TECNIC_Operaciones1', FILENAME = 'C:\SQL Data\TECNIC_Operaciones1.ndf',
+  SIZE = 512MB, MAXSIZE = 4096MB, FILEGROWTH = 256MB ) TO FILEGROUP FG_Operaciones;
 GO
+
+
 USE master;
 GO
 ALTER DATABASE InstitutoTECNIC ADD FILEGROUP FG_Seguridad;
@@ -45,16 +56,8 @@ GO
 USE master;
 GO
 ALTER DATABASE InstitutoTECNIC ADD FILE
-( NAME = 'TECNIC_Seguridad1', FILENAME = 'C:\SQLData\TECNIC_Seguridad1.ndf',
-  SIZE = 10MB, MAXSIZE = 100MB, FILEGROWTH = 2MB ) TO FILEGROUP FG_Seguridad;
+( NAME = 'TECNIC_Seguridad1', FILENAME = 'C:\SQL Data\TECNIC_Seguridad1.ndf',
+  SIZE = 128MB, MAXSIZE = 1024MB, FILEGROWTH = 64MB ) TO FILEGROUP FG_Seguridad;
 GO
-USE master;
-GO
-ALTER DATABASE InstitutoTECNIC ADD FILEGROUP FG_Indices;
-GO
-USE master;
-GO
-ALTER DATABASE InstitutoTECNIC ADD FILE
-( NAME = 'TECNIC_Indices1', FILENAME = 'C:\SQLData\TECNIC_Indices1.ndf',
-  SIZE = 10MB, MAXSIZE = 100MB, FILEGROWTH = 2MB ) TO FILEGROUP FG_Indices;
-GO
+
+
